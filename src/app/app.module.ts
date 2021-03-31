ca4f322bf9ae62d79681efb7850d7db08989486f
juli59 <molnarjuli59@mail.com>
2021-03-31 16:58:33 +0200

    componennts services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AdminComponent } from './admin/admin.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { QuestionEditComponent } from './question-edit/question-edit.component';
+import { QuestionEditorComponent } from './question-editor/question-editor.component';
+import { QuizEditorComponent } from './quiz-editor/quiz-editor.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AdminComponent,
+    QuizComponent,
+    QuestionEditComponent,
+    QuestionEditorComponent,
+    QuizEditorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
